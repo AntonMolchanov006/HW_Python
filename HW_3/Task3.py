e9870c87ba07812ec5d98481aa6cdeff0766932e
AntonMolchanov <a.molchanov886@gmail.com>
2022-05-28 11:16:53 +0300

    task 3 done

--- HW_3/Task3.py
+++ HW_3/Task3.py
@@ -4,3 +4,24 @@
 # Пример:
 
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+
+
+from os import remove
+
+
+start_list = [1.1, 1.2, 3.1, 5, 10.01]
+def Fraction_difference(lst: list) -> float:
+    minimum = 1
+    maximum = 0
+    for i in range(len(lst)):
+        fraction = round((lst[i] - lst[i]//1), 2)
+        if fraction == 0:
+            continue
+        if fraction > maximum:
+            maximum = fraction
+        if fraction < minimum:
+            minimum = fraction
+    return(maximum - minimum)
+
+print(Fraction_difference(start_list))
